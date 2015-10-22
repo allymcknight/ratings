8a1e7055beaa39e000520e53e09dd41a29ad4cbd
Hackbright Student <info@hackbrightacademy.com>
2015-10-21 18:29:14 -0700

    completed login and made user-info.html with @app.route

--- server.py
+++ server.py
@@ -31,6 +31,22 @@ def user_list():
     users = User.query.all()
     return render_template("users_list.html", users=users)
 
+@app.route("/users/<int:user_id>")
+def user_info(user_id):
+    """Shows information about a specific user."""
+
+    user = User.query.filter(User.user_id == user_id).first()
+    # list of rating objects with Rating attributes rating_id, movie_id, user_id, score
+    ratings = user.ratings
+    users_movie_ratings = []
+    # for index in length rating list:
+    # for i in ratings:
+    #     count = len(range(ratings))
+    # #  append to the users_movie_ratings list the movie title at index and the score at that index
+        
+    #     users_movie_ratings.append(ratings[count].movie.title + ratings[count].score)
+    return render_template("user-info.html", user=user)
+
 @app.route("/login")
 def login_form():
     """Render login form"""
@@ -42,17 +58,31 @@ def login_submission():
     """Confirms username and password through database and sessions."""
     # .get is safety net incase 'required' is removed from form in html
     # checks the forms dict which is nested inside the request dictionary
-    email = request.forms.get("email")
-    password = request.forms.get("password")
+    email = request.form.get("email")
+    password = request.form.get("password")
     user = User.query.filter(User.email == email).first()
+    # if user in == True:
     if user:
         # TODO check pw
+        if password == user.password:
         # 1 add user to session
-        # flash message and them redirect "/"
-
+            session["current_user"] = email
+        # flash message 
+            flash("Logged in as %s" % (email))
+        # and them redirect "/"
+            return redirect("/") 
+        else:
+            flash("Incorrect password submitted")
+            return redirect("/login")  
     else:
-        # add user and pw to database
-        # flash message and them redirect "/"
+        # add user and pw to database (instantiation of the class User), add and commit to the database
+        new_user = User(email=email, password=password, age="NULL", zipcode="NULL")
+        db.session.add(new_user)
+        db.session.commit()
+        # flash message 
+        flash("New user logged in as %s" % (email))
+        # and then redirect "/"
+        return redirect("/")
 
 
 

2904f757253453f45a75851830a138a7a9623d8f
Hackbright Student <info@hackbrightacademy.com>
2015-10-22 17:51:22 -0700

    Part 2 complete

--- server.py
+++ server.py
@@ -24,6 +24,54 @@ def index():
 
     return render_template("homepage.html")
 
+@app.route('/movies')
+def movie_list():
+    """Shows list of movies"""
+
+    movies = Movie.query.order_by(Movie.title).all()
+    return render_template("movie-list.html", movies=movies )
+
+@app.route("/movies/<int:movie_id>")
+def movie_info(movie_id):
+    """Show movie information and allow user to add or update rating"""
+
+    movie = Movie.query.filter(Movie.movie_id == movie_id).first()
+    ratings = movie.ratings
+
+    movie_ratings = []
+
+    for obj in ratings:
+        movie_ratings.append(obj.score) 
+      
+    return render_template("movie-info.html", movie_ratings=movie_ratings, movie=movie)
+
+@app.route("/rating_confirmation/<int:movie_id>", methods=["POST"])
+def rating_confirmation(movie_id):
+    """Adds or updates new movie rating to database"""
+
+    score =request.form.get("score")
+    user = session["current_user"]
+    user_obj = User.query.filter(User.email == user).first()
+    user_id = user_obj.user_id
+    movie_obj = Movie.query.filter(Movie.movie_id == movie_id).first()
+    title = movie_obj.title
+
+    rating_obj = Rating.query.filter((Rating.user_id == user_id)
+                                         & (Rating.movie_id == movie_id)).first()
+
+    if rating_obj:
+        # single_rating = rating_obj.query.filter(Movie.movie_id = movie_id).first()
+        rating_obj.score = score
+        db.session.commit()
+        flash("Updated score for %s"%(title))
+    else:   
+        new_rating = Rating(movie_id=movie_id, user_id=user_id, score=score)
+        db.session.add(new_rating)
+        db.session.commit()
+
+        flash("New score for %s" % (title))
+    return redirect("/")
+
 @app.route("/users")
 def user_list():
     """Show list of users"""
@@ -39,13 +87,14 @@ def user_info(user_id):
     # list of rating objects with Rating attributes rating_id, movie_id, user_id, score
     ratings = user.ratings
     users_movie_ratings = []
-    # for index in length rating list:
-    # for i in ratings:
-    #     count = len(range(ratings))
-    # #  append to the users_movie_ratings list the movie title at index and the score at that index
-        
-    #     users_movie_ratings.append(ratings[count].movie.title + ratings[count].score)
-    return render_template("user-info.html", user=user)
+    # here we are finding the number of ratings and using that index get title and score
+    for i in range(len(ratings)):
+        users_movie_ratings.append(ratings[i].movie.title + str(ratings[i].score))
+    # append to the users_movie_ratings list the movie title at index and the score at that index
+    # for obj in ratings:
+        # users_movie_ratings.append(obj.movie.title + str(obj.score))
+
+    return render_template("user-info.html", user=user, users_movie_ratings=users_movie_ratings)
 
 @app.route("/login")
 def login_form():
